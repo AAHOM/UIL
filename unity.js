51fdca23d4d8aca5719a759851df51fa68ea7e3a
George <georgecrane@mac.com>
2022-02-28 10:41:49 -0500

    Update unity.js

--- unity.js
+++ unity.js
@@ -119,47 +119,6 @@ async function fetchGoogleDataAll(urls) {
   return [data3,status];
 }
 
-/*------------------------------------------------------------ */
-/* GetCookie                                                   */
-/*    Split cookie string and get all individual               */
-/*    name=value pairs in an array                             */
-/* ----------------------------------------------------------- */  
-
-function getCookie(name) {
-    
-  // Split cookie string and get all individual name=value pairs in an array
-  var cookieArr = document.cookie.split(";");
-  // Loop through the array elements
-  for(var i = 0; i < cookieArr.length; i++) {
-      var cookiePair = cookieArr[i].split("=");
-      
-      /* Removing whitespace at the beginning of the cookie name
-      and compare it with the given string */
-      if(name == cookiePair[0].trim()) {
-          // Decode the cookie value and return
-          return decodeURIComponent(cookiePair[1]);
-      }
-  }
-  // Return null if not found
-  return null;
-}
-
-function getCachedSheet(name,url) {
-  var useName = name + "Unity";
-  var theCookie = getCookie(useName);
-  var data = sessionStorage.getItem(useName)
-  if (theCookie && data != null) { // cookie still alive
-    var temp = JSON.parse(data); 
-    return temp;
-  }
-  else {
-    var retlist = get_spreadsheet(url);
-    sessionStorage.setItem(useName,JSON.stringify(retlist));
-    document.cookie = useName + "=cookieValue; max-age=" + 5*60 + "; path=/;";
-    return retlist; 
-  }
-}
-
 /* ----------------------------------------------------------- */
 /* Process the ajax request to get spreadsheet data            */
 /*    04/10/2021 - initial                                     */
