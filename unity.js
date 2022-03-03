118f5578c4a9a408fc2dab794b39f2f5af96f3c2
George <georgecrane@mac.com>
2022-03-03 06:45:57 -0500

    Update unity.js

--- unity.js
+++ unity.js
@@ -375,46 +375,54 @@ function build_flipcards(selectorID, boxNumber = '1') {
 /*-------------------------------------------------------------*/
 /* Address/Hours/Admissions flex boxes                         */
 /*    04/10/2021 - initial                                     */
+/*    Updated 03/03/2022                                       */
 /*-------------------------------------------------------------*/
 
-function showAddressInfo(
-  museum = null,
-  file_id = '1eBU2TqbjAT0-PUkKVa0J9obsoyIBJ7ib_KJMQLNym8Y', 
-  sheet = 'Hours') {
+function showAddressInfo(selectorID, museum = 'aahom') {
 
-  museum = museum.toLowerCase();
-  var qry = "SELECT *  WHERE A = '" + museum + "' ORDER BY A, B";
-  var url = 'https://docs.google.com/spreadsheets/u/0/d/'
+    var file_id = '1eBU2TqbjAT0-PUkKVa0J9obsoyIBJ7ib_KJMQLNym8Y'; 
+    var sheet = 'Hours';
+
+    museum = museum.toLowerCase();
+    var qry = "SELECT *  WHERE A = '" + museum + "' ORDER BY A, B";
+    var url = 'https://docs.google.com/spreadsheets/u/0/d/'
     + file_id + '/gviz/tq?tqx=out:json&sheet=' + sheet + 
     '&headers=1&tq=' + escape(qry);
-   // alert(url);
-  var colorClass = "museum" + museum.charAt(0).toUpperCase() + museum.slice(1);
-  $('#hoursContainer').html(out).addClass(colorClass); 
+    // alert(url);
+    var colorClass = "museum" + museum.charAt(0).toUpperCase() + museum.slice(1);
 
-  //var addlist = getCachedSheet('hours',url); 
-  var addlist = get_spreadsheet(url); 
-  var adds = addlist.table.rows;
-  var out = '<p>No data found</p>'; 
-  for (i = 0; i < adds.length; i++) {
-    if (adds[i] && adds[i].c[0] != null && adds[i].c[0].v == museum) {
-      if (adds[0]) {
-        var item = adds[0];  
-        var namd = item.c[0].v;
-        
-        var add1 = (item.c[1] != null) ? item.c[1].v : 'unknown';
-        var text1 = (item.c[2] != null) ? item.c[2].v : 'unknown';
-        var add2 = (item.c[3] != null) ? item.c[3].v : 'unknown';
-        var text2 = (item.c[4] != null) ? item.c[4].v : 'unknown';
-        var add3 = (item.c[5] != null) ? item.c[5].v : 'unknown';
-        var text3 = (item.c[6] != null) ? item.c[6].v : 'unknown';
-        out = '<div>\n<h3>' + add1 + '</h3>\n' + text1 + '</div>\n';
-        out = out + '<div>\n<h3>' + add2 + '</h3>\n' + text2 + '</div>\n';
-        out = out + '<div>\n<h3>' + add3 + '</h3>\n' + text3 + '</div>\n';
-      } 
-      $('#hoursContainer').html(out).css('display','flex'); 
-    }
-  }
-  return; 
+    $(selectorID).addClass(colorClass).addClass('hoursContainer'); 
+
+     // Fetch the spreadsheet data 
+    fetchGoogleDataAll([url]).then(dataArrayx => {
+        if (dataArrayx[1]) {  // if there was a status error of some kind
+            jQuery('#classList .gallery-items')
+            .html('<div class="errorMessage">Error fetching spreadsheet, status= ' + dataArrayx[1] + ' try refreshing page</div>');
+            return; 
+        }
+        var adds = dataArrayx[0][0].table.rows;
+        var out = '<p>No data found</p>'; 
+        for (i = 0; i < adds.length; i++) {
+            if (adds[i] && adds[i].c[0] != null && adds[i].c[0].v == museum) {
+              if (adds[0]) {
+                var item = adds[0];  
+                var namd = item.c[0].v;
+                
+                var add1 = (item.c[1] != null) ? item.c[1].v : 'unknown';
+                var text1 = (item.c[2] != null) ? item.c[2].v : 'unknown';
+                var add2 = (item.c[3] != null) ? item.c[3].v : 'unknown';
+                var text2 = (item.c[4] != null) ? item.c[4].v : 'unknown';
+                var add3 = (item.c[5] != null) ? item.c[5].v : 'unknown';
+                var text3 = (item.c[6] != null) ? item.c[6].v : 'unknown';
+                out = '<div>\n<h3>' + add1 + '</h3>\n' + text1 + '</div>\n';
+                out = out + '<div>\n<h3>' + add2 + '</h3>\n' + text2 + '</div>\n';
+                out = out + '<div>\n<h3>' + add3 + '</h3>\n' + text3 + '</div>\n';
+              } 
+              $(selectorID).html(out).css('display','flex'); 
+            }
+        }
+        return; 
+    });
 }
 
 /*-------------------------------------------------------------*/
